18a542b15f30b54ca5b03b87d5eeef2284d4bae8
Netanel Avshalom <avshalom999@gmail.com>
2025-12-17 14:11:12 +0200

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -13,4 +14,5 @@ def alert():
     return jsonify({"status": "ok"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
